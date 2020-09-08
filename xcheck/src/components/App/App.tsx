98b8e7373929c0a81a1c5d22c6388f24b8685028
Alexandra Burdina <sunh2oalex@gmail.com>
2020-09-09 00:08:45 +0300

    feat: add authorization and registration forms

--- xcheck/src/components/App/App.tsx
+++ xcheck/src/components/App/App.tsx
@@ -1,9 +1,11 @@
 import React from 'react';
+import AuthorizationForm from '../AuthorizationComponent/Authorization&RegistrationForm'
 
 export class App extends React.Component<{},{}> {
   render(){
     return(
     <div className = "App">
+      <AuthorizationForm type='auth'/>
     </div>
   )
   }
